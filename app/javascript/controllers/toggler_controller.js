34f335a3b7d5062667bd73cbf247bc6a38475334
Taimoor Haider <taimoorhaiderx@gmail.com>
2024-09-20 20:36:41 +0500

    Done toggle

--- app/javascript/controllers/toggler_controller.js
+++ app/javascript/controllers/toggler_controller.js
@@ -2,18 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggler"
 export default class extends Controller {
-  static targets=['users'];
-  static classes=["show"]
+  static targets=['usersContainer', 'hiddenUser'];
 
   static values = {
     isVisible: {type:Boolean ,default:false}
   }
 
-
-  isVisibleValueChanged(){
-    this.element.classList.add(this.showClass)
-    console.log('Value changed:', this.isVisibleValue)
+  isVisibleValueChanged(value) {
+    console.log(this.hiddenUserTargets);
+    this.hiddenUserTargets.forEach(el => el.classList.toggle('d-none', value));
   }
+
+  hiddenUserTargetConnected(target)  {
+    target.classList.add('d-none')
+  }
+
   toggleVisibility() {
     this.isVisibleValue =!this.isVisibleValue;
     // this.usersTarget.forEach(user => user.classList.toggle("hidden"));
